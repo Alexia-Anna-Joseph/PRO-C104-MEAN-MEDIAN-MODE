34778a6d3dec7289075a817a5fabaa034c88aa5f
Alexia-Anna-Joseph <78249362+Alexia-Anna-Joseph@users.noreply.github.com>
2022-04-09 20:38:47 +0530

    Update MMM.py

--- MMM.py
+++ MMM.py
@@ -52,6 +52,13 @@ mode_data_for_range = {
                         "75-85": 0,
                         "85-95": 0,
                         "95-105": 0,
+	                "105-115": 0,
+	                "115-125": 0,
+	                "125-135": 0,
+	                "135-145": 0,
+	                "145-155": 0,
+	                "155-165": 0,
+	                "165-175": 0,
 	
                     }
 for height, occurence in data.items():
@@ -61,8 +68,20 @@ for height, occurence in data.items():
         mode_data_for_range["85-95"] += occurence
     elif 95 < float(height) < 105:
         mode_data_for_range["95-105"] += occurence
-    elif 95 < float(height) < 105:
-        mode_data_for_range["95-105"] += occurence
+    elif 105 < float(height) < 115:
+        mode_data_for_range["105-115"] += occurence
+	elif 115 < float(height) < 125:
+        mode_data_for_range["115-125"] += occurence
+	elif 125 < float(height) < 135:
+        mode_data_for_range["125-135"] += occurence
+	elif 135 < float(height) < 145:
+        mode_data_for_range["135-145"] += occurence
+	elif 145 < float(height) < 155:
+        mode_data_for_range["145-155"] += occurence
+	elif 155 < float(height) < 165:
+        mode_data_for_range["155-165"] += occurence
+	elif 165 < float(height) < 175:
+        mode_data_for_range["165-175"] += occurence
 
 mode_range, mode_occurence = 0, 0
 for range, occurence in mode_data_for_range.items():
